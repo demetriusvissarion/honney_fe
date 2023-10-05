fce5fd3dab0e37335b64a4211c44f9ff3fe7b38d
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-10-05 16:31:37 +0100

    background

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import "./App.css";
 import { TodoPage } from "./Pages/TodoPage";
 import { Show } from "./Pages/Show";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import backgroundImage from "./background.avif";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<TodoPage />} />
-          <Route path="/:id" element={<Show />} />
-        </Routes>
-      </Router>
+      <div
+        class="image"
+        style={{
+          height: "400px",
+          width: "550px",
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          display: "block",
+          "margin-left": "auto",
+          "margin-right": "auto",
+        }}
+      >
+        <h1>Todos</h1>
+        <Router>
+          <Routes>
+            <Route path="/" element={<TodoPage />} />
+            <Route path="/:id" element={<Show />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
