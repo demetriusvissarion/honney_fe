b9420c0084cb2cbc2ee25d2e900dc2be47873d51
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-10-05 16:57:29 +0100

    refactored style

--- src/App.js
+++ src/App.js
@@ -3,24 +3,11 @@ import "./App.css";
 import { TodoPage } from "./Pages/TodoPage";
 import { Show } from "./Pages/Show";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import backgroundImage from "./background.avif";
 
 function App() {
   return (
     <div className="App">
-      <div
-        class="image"
-        style={{
-          height: "400px",
-          width: "550px",
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          display: "block",
-          "margin-left": "auto",
-          "margin-right": "auto",
-        }}
-      >
+      <div class="image">
         <h1>Todos</h1>
         <Router>
           <Routes>
